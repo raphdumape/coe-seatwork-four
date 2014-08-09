39a84afb5581d9e846e5a20383fe2edf34f3ff69
mmojagan <kelelel012310@gmail.com>
2014-08-09 14:47:43 +0800

    add sw9

---
+++ sw9.js
@@ -0,0 +1,13 @@
+function isLeapyear(year){
+	if (year%4==0){
+
+		console.log(year + " is a leap year.");
+	}
+	else {
+
+		console.log(year +" is not a leap year");
+	}
+}
+isLeapyear(2014);
+isLeapyear(2013);
+isLeapyear(2012);
